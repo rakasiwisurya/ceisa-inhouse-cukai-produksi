73cae63bcdf7a401fba0a811d5d51631d2c05c59
rakasiwisurya <rakasiwisurya.nutech@gmail.com>
2023-08-02 14:43:40 +0700

    comment chipper.js

--- src/utils/Chiper.js
+++ src/utils/Chiper.js
@@ -1,32 +1,32 @@
 // eslint-disable-next-line
-'use strict';
+// 'use strict';
 
-const crypto = require('crypto');
-const {REACT_APP_KC_SECRETKEY} = process.env
-const key = crypto.createHash('sha256').update(String(REACT_APP_KC_SECRETKEY)).digest('base64').substr(0, 32);
-const ENCRYPTION_KEY = key;
-const IV_LENGTH = 16;
+// const crypto = require('crypto');
+// const {REACT_APP_KC_SECRETKEY} = process.env
+// const key = crypto.createHash('sha256').update(String(REACT_APP_KC_SECRETKEY)).digest('base64').substr(0, 32);
+// const ENCRYPTION_KEY = key;
+// const IV_LENGTH = 16;
 
-function encrypt(text) {
-  let iv = crypto.randomBytes(IV_LENGTH);
-  let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
-  let encrypted = cipher.update(text);
+// function encrypt(text) {
+//   let iv = crypto.randomBytes(IV_LENGTH);
+//   let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+//   let encrypted = cipher.update(text);
 
-  encrypted = Buffer.concat([encrypted, cipher.final()]);
+//   encrypted = Buffer.concat([encrypted, cipher.final()]);
 
-  return iv.toString('hex') + ':' + encrypted.toString('hex');
-}
+//   return iv.toString('hex') + ':' + encrypted.toString('hex');
+// }
 
-function decrypt(text) {
-  let textParts = text.split(':');
-  let iv = Buffer.from(textParts.shift(), 'hex');
-  let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
-  let decrypted = decipher.update(encryptedText);
+// function decrypt(text) {
+//   let textParts = text.split(':');
+//   let iv = Buffer.from(textParts.shift(), 'hex');
+//   let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+//   let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+//   let decrypted = decipher.update(encryptedText);
 
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
+//   decrypted = Buffer.concat([decrypted, decipher.final()]);
 
-  return decrypted.toString();
-}
+//   return decrypted.toString();
+// }
 
-export {encrypt, decrypt}
+// export {encrypt, decrypt}
