b24ca82f105a86025cb12dc19a29ff2021e587ef
rakasiwisurya <rakasiwisurya.nutech@gmail.com>
2023-10-23 11:42:35 +0700

    try to fixing security hotspot sonarqube in chipper and setupProxy

--- src/utils/Chiper.js
+++ src/utils/Chiper.js
@@ -1,17 +1,12 @@
 import crypto from "crypto";
 
-const secretKey = process.env.REACT_APP_KC_SECRETKEY; // Retrieve the secret key from environment variables
-if (!secretKey) {
-  throw new Error("REACT_APP_KC_SECRETKEY is not defined in the environment.");
-}
-
-const ENCRYPTION_KEY = crypto.createHash("sha256").update(secretKey).digest("base64").substr(0, 32);
+const { REACT_APP_ENCRYPTION_KEY } = process.env;
 
 // const IV_LENGTH = 16;
 
 // function encrypt(text) {
 //   let iv = crypto.randomBytes(IV_LENGTH);
-//   let cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+//   let cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(REACT_APP_ENCRYPTION_KEY), iv);
 //   let encrypted = cipher.update(text);
 
 //   encrypted = Buffer.concat([encrypted, cipher.final()]);
@@ -23,12 +18,10 @@ function decrypt(text) {
   let textParts = text.split(":");
   let iv = Buffer.from(textParts.shift(), "hex");
   let encryptedText = Buffer.from(textParts.join(":"), "hex");
-  let decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(REACT_APP_ENCRYPTION_KEY), iv);
   let decrypted = decipher.update(encryptedText);
 
   decrypted = Buffer.concat([decrypted, decipher.final()]);
-
-  console.log("ENCRYPTION_KEY", ENCRYPTION_KEY);
 
   return decrypted.toString();
 }
