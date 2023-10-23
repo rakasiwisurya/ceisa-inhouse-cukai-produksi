9c36ca8641458a3b1f5c5eaa5f41b8429766078f
rakasiwisurya <rakasiwisurya.nutech@gmail.com>
2023-10-23 11:52:02 +0700

    done fixing security hotspot and vulnerability and bug

--- src/utils/Chiper.js
+++ src/utils/Chiper.js
@@ -2,18 +2,6 @@ import crypto from "crypto";
 
 const { REACT_APP_ENCRYPTION_KEY } = process.env;
 
-// const IV_LENGTH = 16;
-
-// function encrypt(text) {
-//   let iv = crypto.randomBytes(IV_LENGTH);
-//   let cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(REACT_APP_ENCRYPTION_KEY), iv);
-//   let encrypted = cipher.update(text);
-
-//   encrypted = Buffer.concat([encrypted, cipher.final()]);
-
-//   return iv.toString("hex") + ":" + encrypted.toString("hex");
-// }
-
 function decrypt(text) {
   let textParts = text.split(":");
   let iv = Buffer.from(textParts.shift(), "hex");
@@ -26,7 +14,4 @@ function decrypt(text) {
   return decrypted.toString();
 }
 
-export {
-  // encrypt,
-  decrypt,
-};
+export { decrypt };
